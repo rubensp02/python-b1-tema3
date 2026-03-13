24eb4e888cee71ae4644bdf4027a4cd9a5ad7dd9
rubensp02 <rubensp02@gmail.com>
2026-03-13 11:54:41 +0100

    HECHO Y CORREGIDO

--- 3a/ej3a2/ej3a2_math_package/geometry.py
+++ 3a/ej3a2/ej3a2_math_package/geometry.py
@@ -1,59 +1,16 @@
 # geometry.py
-
+import math
 
 def square_area(side_length: float) -> float:
-    """
-    Calculate the area of a square.
-
-    Args:
-    - side_length (float): the length of one side of the square.
-
-    Returns:
-    - float: the area of the square.
-    """
-    # Write here your code
-    pass
-
+    return math.pow(side_length, 2)
 
 def rectangle_area(base_length: float, height: float) -> float:
-    """
-    Calculate the area of a rectangle.
-
-    Args:
-    - base_length (float): the length of the base of the rectangle.
-    - height (float): the height of the rectangle.
-
-    Returns:
-    - float: the area of the rectangle.
-    """
-    # Write here your code
-    pass
+    return base_length * height
 
 
 def triangle_area(base_length: float, height: float) -> float:
-    """
-    Calculate the area of a triangle.
-
-    Args:
-    - base_length (float): the length of the base of the triangle.
-    - height (float): the height of the triangle.
-
-    Returns:
-    - float: the area of the triangle.
-    """
-    # Write here your code
-    pass
+    return (base_length * height) / 2
 
 
 def circle_area(radius: float) -> float:
-    """
-    Calculate the area of a circle.
-
-    Args:
-    - radius (float): the radius of the circle.
-
-    Returns:
-    - float: the area of the circle
-    """
-    # Write here your code
-    pass
+    return math.pi * radius ** 2
