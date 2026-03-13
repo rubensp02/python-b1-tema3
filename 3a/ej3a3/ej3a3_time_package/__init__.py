08388d249d200892559d69e5f5c93d18782d493f
rubensp02 <rubensp02@gmail.com>
2026-03-13 15:51:28 +0100

    HECHOY CORREGIDO

--- 3a/ej3a3/ej3a3_time_package/__init__.py
+++ 3a/ej3a3/ej3a3_time_package/__init__.py
@@ -1,4 +1,4 @@
 # write import for convert_date_format
-from .convert_date_format import *
+from .convert_date_format import string_to_datetime, datetime_to_string
 # write import for date_operations
-from .date_operations import *
+from .date_operations import add_days, middle_day_between_two_dates
